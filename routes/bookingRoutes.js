e2d9ba6b1204e8f3dfb696b880a3582793ac7b92
Gagan sodlan <gd885973@gmail.com>
2025-05-05 15:06:36 +0530

    add yml file

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createBooking, getBookings } = require('../controllers/bookingController');
+const { createBooking, getBookings,getMyBookings } = require('../controllers/bookingController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', protect, createBooking);
 router.get('/', protect, getBookings);
+router.get('/my-bookings', protect, getMyBookings);
+
 
 module.exports = router;
